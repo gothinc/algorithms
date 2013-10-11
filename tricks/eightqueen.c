6b1865c0f0a56e1a623c9009ea290ea1131f63ad
z32556601 <zhaojiangwei6464@163.com>
2013-10-11 18:55:45 +0800

    add eightqueen

--- tricks/eightqueen.c
+++ tricks/eightqueen.c
@@ -3,20 +3,70 @@
 #include <string.h>
 
 void printl();
+void place(char *s[][8], int x, int y);
+int is_conflic(char *s[][8], int x, int y);
+int my_abs(int x);
 
 int
 main(int argc, char *argv[]){
 	char *s[8][8];
 	memset(s, 0, sizeof(s));
-	s[1][2] = "x";
-	s[3][4] = "*";
 
+	place(s, 8, 8);
 	printl(s, 8, 8);
 }
 
 void
 place(char *s[][8], int x, int y){
+	int i, j, reverse;
+	for(i = 0, reverse = 0; i < x; i ++){
+		for(j = 0; j < y; j ++){
+			if(reverse){
+				if(s[i][j]){
+					s[i][j] = 0;
+					reverse = 0;
+				}
+			}else{
+				if(!is_conflic(s, i, j)){
+					s[i][j] = "*";
+					break;
+				}
+			}
+		}
 
+		if(j >= y || !s[i][j]){
+			if(i == 0){
+				printf("cannot find\n");
+				break;
+			}
+
+			i -= 2;
+			reverse = 1;
+		}
+	}
+}
+
+int
+is_conflic(char *s[][8], int x, int y){
+	if(x == 0) return 0;
+
+	int i, j;
+	for(i = 0; i < x; i ++){
+		for(j = 0; j < 8; j ++){
+			if(s[i][j]){
+				if(j == y || (x - i) == (my_abs(y - j))) return 1;
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
+
+int
+my_abs(int x){
+	if(x >= 0) return x;
+	return ~x + 1;
 }
 
 void
